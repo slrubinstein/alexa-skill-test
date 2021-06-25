4929cd87cf35310bd7f883c5cf3a26749c0e8730
Steven Rubinstein <stevenrubinstein@Stevens-MacBook-Pro.local>
2021-06-25 15:02:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Welcome to your SDK weather skill. Ask me the weather!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Welcome to your SDK weather skill. Ask me the weather!', speechText)
+      .getResponse();
+  }
+};
+
+const AskWeatherIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWeatherIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'The weather today is sunny.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('The weather today is sunny.', speechText)
+      .getResponse();
+  }
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask me the weather!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('You can ask me the weather!', speechText)
+      .getResponse();
+  }
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Goodbye!', speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    // Any clean-up logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
